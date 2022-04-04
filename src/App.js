61ad57843c822182c9f6a365e067d007c89d009f
jromano67 <89851864+jromano67@users.noreply.github.com>
2022-04-04 12:27:01 -0400

    added Victory

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
-import React, {Component} from 'react';
-import './App.css';
-
+import React, { Component } from "react";
+import "./App.css";
+import Card from "./Components/Card";
+import {
+  VictoryBar,
+  VictoryChart,
+  VictoryAxis,
+  VictoryTheme,
+  VictoryStack
+} from "victory";
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = {}
+    this.state = {};
   }
 
   render() {
     return (
       <div className="App">
         <h1>Health</h1>
+        
+        <VictoryChart />
       </div>
     );
   }
